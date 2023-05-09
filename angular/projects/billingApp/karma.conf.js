fb3aba45c059456557de049f9ffacbcd77d555bf
MarcusRojasPacheco <marcosrojaspacheco@gmail.com>
2023-05-10 01:52:40 +0200

    FICHEROS UT4.PC2

--- angular/projects/billingApp/karma.conf.js
+++ angular/projects/billingApp/karma.conf.js
@@ -25,13 +25,22 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['ChromeHeadless'], // Cambiado a ChromeHeadless
+    // Cambio importante aquí
+    browsers: ['ChromeHeadless'],
+    customLaunchers: {
+      ChromeHeadless: {
+        base: 'Chrome',
+        flags: [
+          '--headless',
+          '--disable-gpu',
+          '--no-sandbox',
+          '--remote-debugging-port=9222',
+        ],
+      },
+    },
     singleRun: false,
     restartOnFileChange: true,
-
     // Aquí agregamos la opción de timeout
     browserNoActivityTimeout: 60000 // 60 segundos
   });
 };
-
-
