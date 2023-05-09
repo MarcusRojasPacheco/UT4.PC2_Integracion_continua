bd1dc4262c7826e574e53c8466589472eb08add1
MarcusRojasPacheco <marcosrojaspacheco@gmail.com>
2023-05-09 23:50:32 +0200

    FICHEROS UT4.PC2

--- angular/projects/billingApp/src/app/app.component.spec.ts
+++ angular/projects/billingApp/src/app/app.component.spec.ts
@@ -36,6 +36,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('billingApp app is running!');
-  });
+    expect(compiled.querySelector('h1').textContent).toContain('Listado de facturas');
+});
 });
