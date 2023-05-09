c89ede102cca4e9ca9002e7887a9fa6cadae1fc9
MarcusRojasPacheco <marcosrojaspacheco@gmail.com>
2023-05-09 22:23:54 +0200

    FICHEROS UT4.PC2

--- angular/projects/billingApp/src/services/swaggerbillingAPI/api.module.ts
+++ angular/projects/billingApp/src/services/swaggerbillingAPI/api.module.ts
@@ -1,36 +1,30 @@
-import { NgModule, ModuleWithProviders, SkipSelf, Optional } from '@angular/core';
+import { NgModule, ModuleWithProviders, Optional, SkipSelf } from '@angular/core';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { Configuration } from './configuration';
-import { HttpClient } from '@angular/common/http';
-import { HttpClientModule } from '@angular/common/http';
-
-
 import { BillingAPIService } from './api/billingAPI.service';
 
 @NgModule({
-    imports:      [ HttpClientModule ],
-    declarations: [],
-    exports:      [],
-    providers: [
-      BillingAPIService
-    ]
-  })
-  
+  imports: [HttpClientModule],
+  declarations: [],
+  exports: [],
+  providers: [BillingAPIService]
+})
 export class ApiModule {
-    public static forRoot(configurationFactory: () => Configuration): ModuleWithProviders<ApiModule> {
-        return {
-            ngModule: ApiModule,
-            providers: [ { provide: Configuration, useFactory: configurationFactory } ]
-        };
+  constructor(@Optional() @SkipSelf() parentModule: ApiModule,
+              @Optional() http: HttpClient) {
+    if (parentModule) {
+      throw new Error('ApiModule is already loaded. Import in your base AppModule only.');
     }
+    if (!http) {
+      throw new Error('You need to import the HttpClientModule in your AppModule! \n' +
+        'See also https://github.com/angular/angular/issues/20575');
+    }
+  }
 
-    constructor( @Optional() @SkipSelf() parentModule: ApiModule,
-                 @Optional() http: HttpClient) {
-        if (parentModule) {
-            throw new Error('ApiModule is already loaded. Import in your base AppModule only.');
-        }
-        if (!http) {
-            throw new Error('You need to import the HttpClientModule in your AppModule! \n' +
-            'See also https://github.com/angular/angular/issues/20575');
-        }
-    }
+  static forRoot(configurationFactory: () => Configuration): ModuleWithProviders<ApiModule> {
+    return {
+      ngModule: ApiModule,
+      providers: [{ provide: Configuration, useFactory: configurationFactory }]
+    };
+  }
 }
