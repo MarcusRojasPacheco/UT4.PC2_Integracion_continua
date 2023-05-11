13ee5547c263ea110f45566ca372a952f3350989
MarcosRojas <34724068+MarcusRojasPacheco@users.noreply.github.com>
2023-05-11 11:28:30 +0200

    Update app.component.spec.ts

--- angular/projects/billingApp/src/app/app.component.spec.ts
+++ angular/projects/billingApp/src/app/app.component.spec.ts
@@ -2,12 +2,13 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { HttpErrorResponse } from '@angular/common/http';  // Importa HttpErrorResponse
 import { AppComponent } from './app.component';
 import { BillingAPIService } from '/var/lib/jenkins/workspace/prueba_0056/angular/projects/billingApp/src/services/swaggerbillingAPI/api/api';
 
 describe('AppComponent', () => {
   let httpMock: HttpTestingController;
-  let billingService: BillingAPIService;  // Se añade el servicio de facturación
+  let billingService: BillingAPIService; 
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -23,7 +24,7 @@ describe('AppComponent', () => {
     }).compileComponents();
 
     httpMock = TestBed.inject(HttpTestingController); 
-    billingService = TestBed.inject(BillingAPIService);  // Se inicializa el servicio de facturación
+    billingService = TestBed.inject(BillingAPIService);
   }));
 
   afterEach(() => {
@@ -37,13 +38,9 @@ describe('AppComponent', () => {
   });
 
   it(`should have as title 'billingApp'`, () => {
-    try {
-      const fixture = TestBed.createComponent(AppComponent);
-      const app = fixture.componentInstance;
-      expect(app.title).toEqual('billingApp');
-    } catch (error) {
-      console.error('Error checking title of AppComponent:', error);
-    }
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('billingApp');
   });
 
   it('should render title', () => {
@@ -69,33 +66,27 @@ describe('BillingAPIService', () => {
   });
 
   afterEach(() => {
-    httpTestingController.verify();  // Ensure that there are no outstanding requests.
+    httpTestingController.verify();
   });
 
   it('should get data', () => {
-    const testData: Data = {name: 'Test Data'};  // Define the data you expect to get from the request.
+    const testData: any = {name: 'Test Data'};  // Cambiar a any si no tienes una interfaz o clase 'Data'
 
-    // Make an HTTP GET request, and expect that it returns the test data.
     service.getData().subscribe(
       data => expect(data).toEqual(testData),
       error => fail('Should have succeeded but failed with ' + error)
     );
 
-    // At this point, the request is pending, and no response has been sent.
-    // The next step is to expect that the request happened, and to simulate the response.
-
-    const req = httpTestingController.expectOne('//billing');  // This is the URL you expect your service to hit.
-
+    const req = httpTestingController.expectOne('//billing'); 
     expect(req.request.method).toEqual('GET');
-
-    req.flush(testData);  // Simulate a response with the test data.
+    req.flush(testData); 
   });
 
   it('should handle http error', () => {
     const emsg = 'deliberate 404 error';
 
     service.getData().subscribe(
-data => fail('Should have failed with the 404 error'),
+      data => fail('Should have failed with the 404 error'),
       (error: HttpErrorResponse) => {
         expect(error.status).toEqual(404, 'status');
         expect(error.error).toEqual(emsg, 'message');
@@ -103,7 +94,6 @@ data => fail('Should have failed with the 404 error'),
     );
 
     const req = httpTestingController.expectOne('//billing');
-
-    req.flush(emsg, { status: 404, statusText: 'Not Found' });  // Simulate an error response.
+    req.flush(emsg, { status: 404, statusText: 'Not Found' });
   });
 });
