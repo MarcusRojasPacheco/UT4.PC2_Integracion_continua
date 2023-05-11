cf717b0a2c0ea01bf5bc42322e30c7dd3c6e6a36
MarcosRojas <34724068+MarcusRojasPacheco@users.noreply.github.com>
2023-05-11 18:25:00 +0200

    Update app.component.spec.ts

--- angular/projects/billingApp/src/app/app.component.spec.ts
+++ angular/projects/billingApp/src/app/app.component.spec.ts
@@ -1,28 +1,35 @@
-// app.component.spec.ts
+import { ReactiveFormsModule } from '@angular/forms';
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { HttpErrorResponse } from '@angular/common/http';  // Importa HttpErrorResponse
 import { AppComponent } from './app.component';
-import { BillingAPIService } from '/var/lib/jenkins/workspace/prueba_0056/angular/projects/billingApp/src/services/swaggerbillingAPI/api/api'; // Asegúrate de importar tu servicio
+import { BillingAPIService } from '/var/lib/jenkins/workspace/prueba_0056/angular/projects/billingApp/src/services/swaggerbillingAPI/api/api';
 
 describe('AppComponent', () => {
-  let billingAPIService: BillingAPIService;
+  let httpMock: HttpTestingController;
+  let billingService: BillingAPIService; 
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        HttpClientTestingModule,
+        ReactiveFormsModule 
       ],
       declarations: [
         AppComponent
       ],
-      providers: [
-        BillingAPIService // Añade tu servicio a la lista de proveedores
-      ]
+      providers: [BillingAPIService], 
     }).compileComponents();
 
-    // Inicializa el servicio utilizando TestBed
-    billingAPIService = TestBed.inject(BillingAPIService);
+    httpMock = TestBed.inject(HttpTestingController); 
+    billingService = TestBed.inject(BillingAPIService);
   }));
+
+  afterEach(() => {
+    httpMock.verify(); 
+  });
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
@@ -40,18 +47,55 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('billingApp app is running!');
+    expect(compiled.querySelector('h1').textContent).toContain('Listado de facturas');
+  });
+});
+
+describe('BillingAPIService', () => {
+  let httpTestingController: HttpTestingController;
+  let service: BillingAPIService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [BillingAPIService],
+    });
+
+    httpTestingController = TestBed.inject(HttpTestingController);
+    service = TestBed.inject(BillingAPIService);
   });
 
-  it('should call the getBillingInformation method on service', () => {
-    // Arranque: Crea un espía para el método que queremos observar
-    const getBillingInformationSpy = spyOn(billingAPIService, 'getBillingInformation');
+  afterEach(() => {
+    httpTestingController.verify();
+  });
 
-    // Act: Llama al método que estamos probando
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
+it('should get data', () => {
+    const testData: any = {name: 'Test Data'}; 
 
-    // Afirmar: Verifica si el método del servicio fue llamado
-    expect(getBillingInformationSpy).toHaveBeenCalled();
+    // Replace 'getData()' with the correct method
+    service.getBillingData().subscribe(
+      data => expect(data).toEqual(testData),
+      error => fail('Should have succeeded but failed with ' + error)
+    );
+
+    const req = httpTestingController.expectOne('//billing'); 
+    expect(req.request.method).toEqual('GET');
+    req.flush(testData); 
+  });
+
+  it('should handle http error', () => {
+    const emsg = 'deliberate 404 error';
+
+    // Replace 'getData()' with the correct method
+    service.getBillingData().subscribe(
+      data => fail('Should have failed with the 404 error'),
+      (error: HttpErrorResponse) => {
+        expect(error.status).toEqual(404, 'status');
+        expect(error.error).toEqual(emsg, 'message');
+      }
+    );
+
+    const req = httpTestingController.expectOne('//billing');
+    req.flush(emsg, { status: 404, statusText: 'Not Found' });
   });
 });
